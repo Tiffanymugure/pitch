65550f695ef439a657f33066e5223391e056b5a2
tiffanymugure <amiratiffany@gmail.com>
2018-11-19 11:54:14 +0300

    class updated

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class TestConfig(Config):
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = os.eynviron.get("DATABASE_URL")
     pass
 
 class DevConfig(Config):
